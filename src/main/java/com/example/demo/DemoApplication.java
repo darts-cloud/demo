e91657ed028e638c02473d1bcba2575239f7651b
darts-cloud <65764941+darts-cloud@users.noreply.github.com>
2024-01-07 13:09:40 +0900

    commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,19 +1,33 @@
 package com.example.demo;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
 
+    static Log log;
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		log = LogFactory.getLog(DemoApplication.class);
+        log.info("ログ出力");
+	}
+
+	@GetMapping("/")
+	public String index() {
+        log.info("ログ出力");
+		return String.format("Hello %s!", "index");
 	}
 
 	@GetMapping("/hello")
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        log.info("ログ出力");
 		return String.format("Hello %s!", name);
 	}
 
